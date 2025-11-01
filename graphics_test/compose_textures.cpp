db2e31ea923f5d7b51508a11e0351eab484d60bc
Julian Mejia <julian102030@gmail.com>
2025-11-01 13:29:29 -0500

    We organized how to access the directories

--- graphics_test/compose_textures.cpp
+++ graphics_test/compose_textures.cpp
@@ -32,9 +32,9 @@ Texture loadRankTexture(Card card){
 
 std::vector<Texture> loadSuitTextures(Card card){
     std::vector<Texture> suitTextures;
-    for (Suit suit : card.getSuits()){
+    for (const auto &dir : card.getSuitTextureDirs()){
         Texture suitTex;
-        if (!suitTex.loadFromFile(card.getSuitTextureDirs(suit))){
+        if (!suitTex.loadFromFile(dir)){
             std::cerr << "Failed to load textures\n";
         }
         suitTextures.push_back(suitTex);
@@ -47,15 +47,18 @@ std::vector<Texture> loadSuitTextures(Card card){
 int main() {
     RenderWindow window(VideoMode(800, 600), "Composited Card");
 
-    Card c1({Suit::SPADES}, Rank::ACE, {Cmod::Wild});
+    Card c1({Suit::SPADES, Suit::HEARTS}, Rank::ACE, {Cmod::Wild});
 
-    Texture cardTex, rankTex, spadeTex;
-    if (!cardTex.loadFromFile("/home/julian/juli/sem8/rummy-latro/art/card/card.png") ||
-        !rankTex.loadFromFile("/home/julian/juli/sem8/rummy-latro/art/rank/A.png") ||
-        !spadeTex.loadFromFile("/home/julian/juli/sem8/rummy-latro/art/suit/SPADES.png")) {
-        std::cerr << "Failed to load textures\n";
-        return -1;
-    }
+    Texture cardTex, rankTex;
+    cardTex = loadCardTexture(c1);
+    rankTex = loadRankTexture(c1);
+    std::vector<Texture> suitTextures = loadSuitTextures(c1);
+    //if (!cardTex.loadFromFile("/home/julian/juli/sem8/rummy-latro/art/card/card.png") ||
+    //    !rankTex.loadFromFile("/home/julian/juli/sem8/rummy-latro/art/rank/A.png") ||
+    //    !spadeTex.loadFromFile("/home/julian/juli/sem8/rummy-latro/art/suit/SPADES.png")) {
+    //    std::cerr << "Failed to load textures\n";
+    //    return -1;
+    //}
 
     ////configure card sprite (base layer)
     Sprite card(cardTex);
@@ -69,7 +72,11 @@ int main() {
     //rank.setColor(sf::Color(0, 0, 0));
 
     ////configure suit sprite (another layer)
-    Sprite suit(spadeTex);
+    std::vector<Sprite> suits;
+    for (const auto &tex : suitTextures){
+        Sprite suit(tex);
+        suits.push_back(suit);
+    }
     //suit.setScale(8.f, 8.f);
     //suit.setPosition(card.getPosition() + Vector2f(20, 80));
     //suit.setColor(sf::Color(0, 0, 0));
@@ -83,14 +90,28 @@ int main() {
     rank.setScale(8.f, 8.f);
     rank.setPosition(10, 10);
     rank.setColor(sf::Color(0, 0, 0));
-    suit.setScale(8.f, 8.f);
-    suit.setPosition(10, 80);
-    suit.setColor(sf::Color(0, 0, 0));
+
+    for(int i=0; i<suits.size(); i++){
+        Sprite &suit = suits[i];
+        suit.setScale(8.f, 8.f);
+        suit.setPosition(10+ i*60, 80); // adjust position for each suit
+        suit.setColor(sf::Color(0, 0, 0));
+        if (i>0){
+            suit.setColor(sf::Color(255, 0, 0));
+        }
+        renderTex.draw(suit);
+    }
+    //suit.setScale(8.f, 8.f);
+    //suit.setPosition(10, 80);
+    //suit.setColor(sf::Color(0, 0, 0));
 
     renderTex.clear(Color::Transparent);
     renderTex.draw(card);
     renderTex.draw(rank);
-    renderTex.draw(suit);
+    for(const auto &suit : suits){
+        renderTex.draw(suit);
+    }
+    //renderTex.draw(suit);
     renderTex.display();
 
     Sprite composedCard(renderTex.getTexture());

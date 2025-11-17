150f7f19d2469623835f009f0c78bf995ddaf104
Julian Mejia <julian102030@gmail.com>
2025-11-17 08:24:58 -0500

    Just added CMakeFiles and changed some directories but all is cool

---
+++ build/CMakeFiles/CardGame.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/julian/juli/sem8/rummy-latro/rummylatro/src/Card.cpp" "CMakeFiles/CardGame.dir/src/Card.cpp.o" "gcc" "CMakeFiles/CardGame.dir/src/Card.cpp.o.d"
+  "/home/julian/juli/sem8/rummy-latro/rummylatro/src/main.cpp" "CMakeFiles/CardGame.dir/src/main.cpp.o" "gcc" "CMakeFiles/CardGame.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- graphics_test/graphics.md
+++
@@ -1,8 +0,0 @@
-# Algunos comentarios sobre las gráficas
-
-la manera en la que está hecho el entorno gráfico respecto a los ejes coordenados es:
-
-(0, 0)-------(x_max, 0)
- |                | 
- |                |
-(0, y_max)---(x_max, y_max)

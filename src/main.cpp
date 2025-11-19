3db1832160a49edd0314bb5022b293d6686e5c29
Julian Mejia <julian102030@gmail.com>
2025-11-19 08:04:52 -0500

    Starting to make the functions that actually make the graphical part

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,28 @@
 #include "Deck.hpp"
 #include <iostream>
 
+//NOW LETS MAKE THE FUNCTION I NEED TO RENDER THE DIFFERENT TYPES OF POSSIBLE LOOKS ON THE Card
+    // draw suit sprites positioned relative to the card
+//void drawCard(){
+//    for (auto &cardsuits : card.getSuits()){
+//        Sprite &s = suitSprites[static_cast<std::size_t>(cardsuits)];
+//        // Example positions; adjust as needed
+//        if (cardsuits == Suit::SPADES) {
+//            s.setPosition(10.f, 80.f);
+//            s.setColor(suitToColor(cardsuits));
+//        } else if (cardsuits == Suit::HEARTS) {
+//            s.setPosition(70.f, 80.f);
+//            s.setColor(suitToColor(cardsuits));
+//        } else if (cardsuits == Suit::DIAMONDS) {
+//            s.setPosition(130.f, 80.f);
+//            s.setColor(suitToColor(cardsuits));
+//        } else if (cardsuits == Suit::CLUBS) {
+//            s.setPosition(190.f, 80.f);
+//            s.setColor(suitToColor(cardsuits));
+//        }
+//        renderTex->draw(s);
+//}
+
 using namespace sf;
 
 std::unique_ptr<sf::RenderTexture> RenderCard(Card& card, float scale, float posX, float posY) {
@@ -19,26 +41,26 @@ std::unique_ptr<sf::RenderTexture> RenderCard(Card& card, float scale, float pos
 
     // Create sprites from those textures
     Sprite cardSprite(cardTex);
-    cardSprite.setScale(8.f, 8.f);
+    cardSprite.setScale(myscale, myscale);
     cardSprite.setPosition(gCard.getPosX(), gCard.getPosY()); // optional: place the baked card origin
    //cardSprite.setColor(suitToColor(card.getFirstSuit()));
 
     Sprite rankSprite(rankTex);
-    rankSprite.setScale(8.f, 8.f);
+    rankSprite.setScale(myscale, myscale);
     rankSprite.setPosition(gCard.getPosX() + 20.f, gCard.getPosY() + 20.f);
     rankSprite.setColor(suitToColor(card.getFirstSuit()));
     std::vector<Sprite> suitSprites;
     suitSprites.reserve(suitTextures.size());
     for (const auto &tex : suitTextures) {
         Sprite s(tex);
-        s.setScale(8.f, 8.f);
+        s.setScale(myscale, myscale);
         suitSprites.push_back(s);
     }
 
     // Create a render texture on the heap and return it via unique_ptr
     auto renderTex = std::make_unique<RenderTexture>();
-    unsigned int width  = cardTex.getSize().x * 8u;
-    unsigned int height = cardTex.getSize().y * 8u;
+    unsigned int width  = cardTex.getSize().x * myscale;//*8u
+    unsigned int height = cardTex.getSize().y * myscale;//*8u
     if (!renderTex->create(width, height)) {
         std::cerr << "Failed to create render texture\n";
         return nullptr; // indicate failure
@@ -86,7 +108,7 @@ int main() {
 
     Card c1({Suit::DIAMONDS, Suit::SPADES, Suit::CLUBS, Suit::HEARTS}, Rank::ACE, {Cmod::Wild});
 
-    auto renderTex = RenderCard(c1, 0.f, 0.f);
+    auto renderTex = RenderCard(c1, 8.f, 0.f, 0.f);
     sf::Sprite bakedCardSprite(renderTex->getTexture());
     bakedCardSprite.setPosition(100.f, 100.f);
 

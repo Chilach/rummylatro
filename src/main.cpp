a4234e7233a4737d27117fc2c10fa26f160c94a8
Julian Mejia <julian102030@gmail.com>
2025-12-06 17:49:10 -0500

    Just started to see how to add the deck

--- src/main.cpp
+++ src/main.cpp
@@ -8,29 +8,19 @@
 using namespace sf;
 
 int main() {
-    RenderWindow window(VideoMode(800, 600), "Composited Card");
-    //Este es un poco de texto de prueba para mostrar lo linda que es mi fuenteikj  
+    
+    //ahora vamos a ver como hacer los Deck y los jugadores
 
-    //Card c1({Suit::DIAMONDS, Suit::SPADES, Suit::CLUBS, Suit::HEARTS}, Rank::ACE, {Cmod::Wild});
-    //Card c1({Suit::DIAMONDS, Suit::SPADES, Suit::CLUBS}, Rank::ACE, {Cmod::Wild});
-    //Card c1({Suit::DIAMONDS}, Rank::ACE, {Cmod::Wild});
-    //Card c1({Suit::SPADES, Suit::DIAMONDS}, Rank::ACE, {Cmod::Wild});
-    //Card c1({Suit::DIAMONDS, Suit::HEARTS}, Rank::ACE, {Cmod::Wild});
-    Card c1({Suit::HEARTS, Suit::JOKERS}, Rank::ACE, {Cmod::Wild});
 
-    auto renderTex = RenderCard(c1, 8.f, 0.f, 0.f);
-    sf::Sprite bakedCardSprite(renderTex->getTexture());
-    bakedCardSprite.setPosition(100.f, 100.f);
+    //NOW WE HAVE TO CREATE THE COMPLETE DECK 
+    Deck tabledeck = Deck::createStandardDeck(); 
+    //TABLEDECK.showDeck();
+    std::cout << "Deck size: " << tabledeck.deckSize() << std::endl;
+    Player julian("julian", tabledeck.dealCards(5));
+    std::cout << julian.getName() << std::endl;
+    julian.showDeck();
+    Player ella("ella", tabledeck.dealCards(5));
+    std::cout << ella.getName() << std::endl;
+    ella.showDeck();
 
-    while (window.isOpen()) {
-        Event e;
-        while (window.pollEvent(e)) {
-            if (e.type == Event::Closed)
-                window.close();
-        }
-
-        window.clear(Color::Blue);
-        window.draw(bakedCardSprite);
-        window.display();
-    }
 }
